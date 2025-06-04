dde1d9874db674b14c826ef6ff6d5a083fe8ccfa
Si Yujun <siuj0810@gmail.com>
2025-06-05 02:23:51 +0900

    design/9: App Colors 수정 완료

--- lib/design_system/color/app_colors.dart
+++ lib/design_system/color/app_colors.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color lavender = Color(0xFFE6E6FA);
+  //메인 사용 컬러
+  static const Color paleLemon = Color(0xFFFFFACD);
+  static const Color lemonYellow = Color(0xFFFFF59D);
   static const Color babyPink = Color(0xFFFFC0CB);
-  static const Color peach = Color(0xFFFFD0DB);
-  static const Color paleLemon = Color(0xFFFFF59D);
-  static const Color powderBlue = Color(0xFFB2DFDB);
+  static const Color peach = Color(0xFFFFDAB9);
 
   static const Color white = Color(0xFFFFFFFF);
   static const Color darkText = Color(0xFF4A4A4A);
   static const Color softBlack = Color(0xFF333333);
+
+  // 사용 예정 컬러
+  static const softViolet = Color(0xFFCDB4DB);
+  static const dustyRose = Color(0xFFD8A7B1);
+  static const slateBlue = Color(0xFF6A5ACD);
+  static const mintGreen = Color(0xFFB2F2BB);
+  static const limeGreen = Color(0xFFC5E384);
+  static const pistachio = Color(0xFFD9ED92);
+  static const coral = Color(0xFFFF6B6B);
+  static const skyBlue = Color(0xFF87CEFA);
+  static const apricot = Color(0xFFFFD8A9);
+  static const tangerine = Color(0xFFFFA94D);
 }
