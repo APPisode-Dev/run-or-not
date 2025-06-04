dc0c26285a1a636e579dfd1f5dc6f7620d1342a1
Si Yujun <siuj0810@gmail.com>
2025-06-05 02:19:03 +0900

    feat/12: home view 작업 완료

--- lib/presentation/home/home_view.dart
+++ lib/presentation/home/home_view.dart
@@ -1,45 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:run_or_not/design_system/button/custom_button.dart';
+import 'package:run_or_not/design_system/color/app_colors.dart';
+import 'package:run_or_not/design_system/text/custom_text_style.dart';
 import 'home_intent.dart';
 import 'home_view_model.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 800),
+      vsync: this,
+    )..repeat(reverse: true);
+    _animation = Tween<double>(
+      begin: 1.0,
+      end: 1.10,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final _viewModel = context.read<HomeViewModel>();
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
+      backgroundColor: AppColors.paleLemon,
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Selector<HomeViewModel, bool>(
-            selector: (context, viewModel) => viewModel.state.isLoading,
-            builder: (context, isLoading, _) {
-              return isLoading
-                  ? const CircularProgressIndicator()
-                  : const SizedBox.shrink();
-            },
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: isPortrait ? 60.0 : 20.0),
+            child: ScaleTransition(
+              scale: _animation,
+              child: Text(
+                '달릴까 말까 !?',
+                textAlign: TextAlign.center,
+                style: CustomTextStyle.heading1.copyWith(
+                  color: AppColors.peach,
+                  fontSize: 48,
+                  shadows: [
+                    Shadow(
+                      offset: const Offset(2.0, 2.0),
+                      blurRadius: 3.0,
+                      color: AppColors.softBlack,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          const SizedBox(height: 16),
-          Selector<HomeViewModel, String>(
-            selector: (context, viewModel) => viewModel.state.message,
-            builder: (context, message, _) {
-              return Text(message);
-            },
-          ),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () =>
-                _viewModel.send(ChangeButtonTapped()),
-            child: const Text("메시지 체인지"),
-          ),
-          ElevatedButton(
-            onPressed: () =>
-                _viewModel.send(NavigateToHomeDetail()),
-            child: const Text("Home Detail View 이동"),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Flexible(
+                    flex: 20,
+                    child: Image.asset(
+                      'assets/images/horse_yellow.png',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  Row(
+                    spacing: 10,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: CustomButton(
+                          child: Text(
+                            '시작하기',
+                            style: CustomTextStyle.buttonLarge,
+                          ),
+                          faceColor: AppColors.peach,
+                          borderRadius: 15,
+                          onPressed: () {
+                            _viewModel.send(NavigateToHomeDetail());
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: CustomButton(
+                          child: Text(
+                            '설정하기',
+                            style: CustomTextStyle.buttonLarge,
+                          ),
+                          faceColor: AppColors.peach,
+                          borderRadius: 15,
+                          onPressed: () {
+                            _viewModel.send(NavigateToHomeDetail());
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
