2c0206ee1375bfb84860f4e048a5fb51b425735e
Si Yujun <siuj0810@gmail.com>
2025-06-05 01:18:34 +0900

    feat: main debug banner 삭제

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -4,23 +4,19 @@ import 'package:run_or_not/core/di/injector.dart';
 
 void main() {
   setupDependencies();
-  runApp(
-    MyApp(router: getIt<GoRouter>()),
-  );
+  runApp(MyApp(router: getIt<GoRouter>()));
 }
 
 class MyApp extends StatelessWidget {
   final GoRouter router;
 
-  const MyApp({
-    super.key,
-    required this.router
-  });
+  const MyApp({super.key, required this.router});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: router,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
