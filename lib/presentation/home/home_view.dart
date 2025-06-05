f79c99096ed7b2a68e59137b510c4e59b72e72d6
Si Yujun <siuj0810@gmail.com>
2025-06-05 16:04:21 +0900

    feat: animated title text home view 적용 완료

--- lib/presentation/home/home_view.dart
+++ lib/presentation/home/home_view.dart
@@ -3,41 +3,12 @@ import 'package:provider/provider.dart';
 import 'package:run_or_not/design_system/button/custom_button.dart';
 import 'package:run_or_not/design_system/color/app_colors.dart';
 import 'package:run_or_not/design_system/text/custom_text_style.dart';
+import 'package:run_or_not/presentation/home/widgets/animated_title_text.dart';
 import 'home_intent.dart';
 import 'home_view_model.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 800),
-      vsync: this,
-    )..repeat(reverse: true);
-    _animation = Tween<double>(
-      begin: 1.0,
-      end: 1.10,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
-
-    _controller.repeat(reverse: true);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,24 +22,7 @@ class _HomeViewState extends State<HomeView>
         children: <Widget>[
           Padding(
             padding: EdgeInsets.only(top: isPortrait ? 60.0 : 20.0),
-            child: ScaleTransition(
-              scale: _animation,
-              child: Text(
-                '달릴까 말까 !?',
-                textAlign: TextAlign.center,
-                style: CustomTextStyle.heading1.copyWith(
-                  color: AppColors.peach,
-                  fontSize: 48,
-                  shadows: [
-                    Shadow(
-                      offset: const Offset(2.0, 2.0),
-                      blurRadius: 3.0,
-                      color: AppColors.softBlack,
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            child: const AnimatedTitleText(),
           ),
           Expanded(
             child: Center(
