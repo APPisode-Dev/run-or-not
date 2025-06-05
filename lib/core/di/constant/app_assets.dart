162d029c9b762ef0c60ff27b9abe8405c5833782
Si Yujun <siuj0810@gmail.com>
2025-06-05 21:47:40 +0900

    feat: app assets class 에 image path static const 값으로 정의

---
+++ lib/core/di/constant/app_assets.dart
@@ -0,0 +1,10 @@
+abstract class AppAssets {
+  // Images
+  static const String _imagesPath = 'assets/images';
+
+  // Characters
+  static const String horseYellow = '$_imagesPath/horse_yellow.png';
+  static const String horseBlue = '$_imagesPath/horse_blue.png';
+  static const String horseRed = '$_imagesPath/horse_red.png';
+  static const String horseGreen = '$_imagesPath/horse_green.png';
+}
