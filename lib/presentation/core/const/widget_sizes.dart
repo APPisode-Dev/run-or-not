f0d027173c790b330eb8c459f8edcf094bfe45f7
kdjun97 <kdjun97@gmail.com>
2025-06-07 21:14:24 +0900

    feat/37: abstract로 class 선언 수정

--- lib/presentation/core/const/widget_sizes.dart
+++ lib/presentation/core/const/widget_sizes.dart
@@ -1,4 +1,4 @@
-class WidgetSizes {
+abstract class WidgetSizes {
   static const double avatarCircleSize = 40;
   static const double rankingContainerHeight = 40;
   static const double gamePlayContainerHeight = 44;
