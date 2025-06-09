b99cef60175f8a0e0dd6d24ac39053fed7cb1762
Si Yujun <siuj0810@gmail.com>
2025-06-09 16:13:31 +0900

    feat: Add Home Detail Intent 추가 완료

--- lib/presentation/home_detail/home_detail_intent.dart
+++ lib/presentation/home_detail/home_detail_intent.dart
@@ -1,3 +1,25 @@
 sealed class HomeDetailIntent {}
 
 class NavigateToGamePlay extends HomeDetailIntent {}
+
+class AddCharacter extends HomeDetailIntent {}
+
+class RemoveCharacter extends HomeDetailIntent {
+  final int index;
+
+  RemoveCharacter(this.index);
+}
+
+class UpdateCharacterName extends HomeDetailIntent {
+  final int index;
+  final String name;
+
+  UpdateCharacterName(this.index, this.name);
+}
+
+class UpdateCharacterImage extends HomeDetailIntent {
+  final int index;
+  final String imagePath;
+
+  UpdateCharacterImage(this.index, this.imagePath);
+}
