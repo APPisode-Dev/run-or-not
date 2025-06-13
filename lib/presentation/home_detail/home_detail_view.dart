2a9536679dcd8b6d98d26187e6fdd054c0482fb1
Si Yujun <siuj0810@gmail.com>
2025-06-13 16:05:13 +0900

    feat: 가로 모드를 위한 공통 위젯 로컬 변수 적용 및 구현 완료

--- lib/presentation/home_detail/home_detail_view.dart
+++ lib/presentation/home_detail/home_detail_view.dart
@@ -4,9 +4,8 @@ import 'package:run_or_not/design_system/color/app_colors.dart';
 import 'package:run_or_not/design_system/text/custom_text_style.dart';
 import 'package:run_or_not/presentation/home_detail/home_detail_intent.dart';
 import 'package:run_or_not/presentation/home_detail/home_detail_view_model.dart';
-import 'package:run_or_not/presentation/home_detail/widgets/character_count_controls.dart';
-import 'package:run_or_not/presentation/home_detail/widgets/character_input_field.dart';
-import 'package:run_or_not/presentation/home_detail/widgets/custom_dialog.dart';
+import 'package:run_or_not/presentation/home_detail/widgets/character_card.dart';
+import 'package:run_or_not/presentation/home_detail/widgets/character_count_stepper.dart';
 import 'package:run_or_not/presentation/home_detail/widgets/start_game_button.dart';
 
 class HomeDetailView extends StatelessWidget {
@@ -14,7 +13,93 @@ class HomeDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    final isPortrait = orientation == Orientation.portrait;
+
+    final titleText = Text(
+      "캐릭터의 이름을 입력하세요\n(10자 이내로)",
+      textAlign: TextAlign.center,
+      style: CustomTextStyle.titleMedium.copyWith(color: AppColors.softBlack),
+    );
+
+    final characterStepper = Selector<HomeDetailViewModel, (int, bool, bool)>(
+      selector:
+          (context, vm) => (
+            vm.state.characterCount,
+            vm.state.canAddCharacter,
+            vm.state.canRemoveCharacter,
+          ),
+      builder: (context, data, _) {
+        final (characterCount, canAdd, canRemove) = data;
+        return CharacterCountStepper(
+          characterCount: characterCount,
+          canAddCharacter: canAdd,
+          canRemoveCharacter: canRemove,
+          onAddCharacter:
+              () => context.read<HomeDetailViewModel>().send(AddCharacter()),
+          onRemoveCharacter:
+              () => context.read<HomeDetailViewModel>().send(
+                RemoveLastCharacter(),
+              ),
+          onCountChanged:
+              (newCount) => context.read<HomeDetailViewModel>().send(
+                SetCharacterCount(newCount),
+              ),
+        );
+      },
+    );
+
+    final characterGrid =
+        Selector<HomeDetailViewModel, (List<String>, List<String>)>(
+          selector:
+              (context, vm) => (
+                vm.state.characterNames,
+                vm.state.characterImages,
+              ),
+          builder: (context, data, _) {
+            final (characterNames, characterImages) = data;
+            return GridView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: isPortrait ? 2 : 3,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: isPortrait ? 1.4 : 1.0,
+              ),
+              itemCount: characterNames.length,
+              itemBuilder: (context, index) {
+                return CharacterCard(
+                  index: index,
+                  currentName: characterNames[index],
+                  imagePath: characterImages[index],
+                  onNameChanged:
+                      (index, name) => context.read<HomeDetailViewModel>().send(
+                        UpdateCharacterName(index, name),
+                      ),
+                  onImageTap: (index) {
+                    // TODO: 향후 SideEffect 패턴 등으로 개선
+                  },
+                );
+              },
+            );
+          },
+        );
+
+    final startGameButton = Selector<HomeDetailViewModel, bool>(
+      selector: (context, vm) => vm.state.canStartGame,
+      builder: (context, canStartGame, _) {
+        return StartGameButton(
+          canStartGame: canStartGame,
+          onStartGame:
+              () => context.read<HomeDetailViewModel>().send(
+                NavigateToGamePlay(),
+              ),
+        );
+      },
+    );
+
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.paleLemon,
       appBar: AppBar(
         title: Text(
@@ -28,129 +113,40 @@ class HomeDetailView extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              "캐릭터의 이름을 입력하세요 \n(10자 이내로)",
-              style: CustomTextStyle.titleMedium.copyWith(
-                color: AppColors.softBlack,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 32),
-            Selector<HomeDetailViewModel, (int, bool, bool)>(
-              selector:
-                  (context, viewModel) => (
-                    viewModel.state.characterCount,
-                    viewModel.state.canAddCharacter,
-                    viewModel.state.canRemoveCharacter,
-                  ),
-              builder: (context, data, _) {
-                final (characterCount, canAdd, canRemove) = data;
-                return CharacterCountControls(
-                  characterCount: characterCount,
-                  canAddCharacter: canAdd,
-                  canRemoveCharacter: canRemove,
-                  onAddCharacter:
-                      () => context.read<HomeDetailViewModel>().send(
-                        AddCharacter(),
+        child:
+            isPortrait
+                ? Column(
+                  children: [
+                    titleText,
+                    const SizedBox(height: 32),
+                    characterStepper,
+                    const SizedBox(height: 16),
+                    Expanded(child: characterGrid),
+                    const SizedBox(height: 24),
+                    startGameButton,
+                  ],
+                )
+                : Row(
+                  children: [
+                    SizedBox(
+                      width: 250,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          titleText,
+                          const SizedBox(height: 32),
+                          characterStepper,
+                          const Spacer(),
+                          startGameButton,
+                          const SizedBox(height: 16),
+                        ],
                       ),
-                  onRemoveCharacter:
-                      () => context.read<HomeDetailViewModel>().send(
-                        RemoveLastCharacter(),
-                      ),
-                  onCountChanged: (newCount) {
-                    context.read<HomeDetailViewModel>().send(
-                      SetCharacterCount(newCount),
-                    );
-                  },
-                );
-              },
-            ),
-            const SizedBox(height: 16),
-            Expanded(
-              child:
-                  Selector<HomeDetailViewModel, (List<String>, List<String>)>(
-                    selector:
-                        (context, viewModel) => (
-                          viewModel.state.characterNames,
-                          viewModel.state.characterImages,
-                        ),
-                    builder: (context, data, _) {
-                      final (characterNames, characterImages) = data;
-
-                      return GridView.builder(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 12,
-                              mainAxisSpacing: 12,
-                              childAspectRatio: 1.4,
-                            ),
-                        itemCount: characterNames.length,
-                        itemBuilder: (context, index) {
-                          return CharacterInputField(
-                            index: index,
-                            currentName: characterNames[index],
-                            imagePath: characterImages[index],
-                            onNameChanged: (index, name) {
-                              context.read<HomeDetailViewModel>().send(
-                                UpdateCharacterName(index, name),
-                              );
-                            },
-                            onImageTap:
-                                (index) => _showCharacterSelector(
-                                  context,
-                                  index,
-                                  context.read<HomeDetailViewModel>(),
-                                ),
-                            onRemove:
-                                index >= 2
-                                    ? () => context
-                                        .read<HomeDetailViewModel>()
-                                        .send(RemoveCharacter(index))
-                                    : null,
-                          );
-                        },
-                      );
-                    },
-                  ),
-            ),
-            const SizedBox(height: 24),
-            Selector<HomeDetailViewModel, bool>(
-              selector: (context, viewModel) => (viewModel.state.canStartGame),
-              builder: (context, data, _) {
-                final canStartGame = data;
-                return StartGameButton(
-                  canStartGame: canStartGame,
-                  onStartGame:
-                      () => context.read<HomeDetailViewModel>().send(
-                        NavigateToGamePlay(),
-                      ),
-                );
-              },
-            ),
-          ],
-        ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(child: characterGrid),
+                  ],
+                ),
       ),
     );
-  }
-
-  void _showCharacterSelector(
-    BuildContext context,
-    int index,
-    HomeDetailViewModel viewModel,
-  ) async {
-    final currentImage = viewModel.state.characterImages[index];
-
-    final result = await CustomDialog.showCharacterSelectorDialog(
-      context: context,
-      currentCharacter: currentImage,
-    );
-
-    if (result != null) {
-      viewModel.send(UpdateCharacterImage(index, result));
-    }
   }
 }
